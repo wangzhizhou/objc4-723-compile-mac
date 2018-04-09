c7157de3ab954fcefc269e5ba153bce22e32a70d
wangzhizhou <824219521@qq.com>
2018-04-09 19:33:29 +0800

    complete the objc4-723 runtime lib

---
+++ debug-objc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  debug-objc
+//
+//  Created by JokerAtBaoFeng on 2018/4/9.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
